3247c2ea2af0dafff390e4756fc64c4eb0ccc078
Furkan Paşaoğlu <16624085+furkanpasaoglu@users.noreply.github.com>
2021-02-11 00:00:59 +0300

    10 Şubat 3.commit

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -6,5 +6,7 @@
         //public varsa = Pascal Case 
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        internal static string ProductsListed = "Ürünler listelendi";
     }
 }
