496b6e2f29a354f1dd3629dcc6e9d4016ea8c8ca
Furkan Paşaoğlu <16624085+furkanpasaoglu@users.noreply.github.com>
2021-02-10 23:35:09 +0300

    10 Şubat 2. commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -5,6 +5,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Business.Constants;
 using Core.Utilities.Results;
 using Entities.DTOs;
 
@@ -19,12 +20,14 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        public List<Product> GetAll()
+        public IDataResult<List<Product>> GetAll()
         {
-            //İş Kodları
-            //Yetkisi Var mı?
+            if (DateTime.Now()==22)
+            {
+                return new ErrorDataResult();
+            }
 
-            return _productDal.GetAll();
+            return new SuccessDataResult<List<Product>>( _productDal.GetAll(),true,"Ürünler Listelendi");
         }
 
         public List<Product> GetAllByCategory(int id)
@@ -52,11 +55,12 @@ namespace Business.Concrete
             //business codes
             if (product.ProductName.Length<2)
             {
-                return new ErrorResult("Ürün ismi en az 2 karakter olmalıdır");
+                //magic strings
+                return new ErrorResult(Messages.ProductNameInvalid);
             }
             _productDal.Add(product);
 
-            return new Result(true,"Ürün Eklendi");
+            return new SuccessResult(Messages.ProductAdded);
         }
     }
 }
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,10 @@
+﻿namespace Business.Constants
+{
+    //constants = sabit
+    public static class Messages
+    {
+        //public varsa = Pascal Case 
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+    }
+}
