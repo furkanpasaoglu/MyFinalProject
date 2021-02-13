3aecdd3f98cb0e053ff806885903aa5c43d23660
Furkan Paşaoğlu <16624085+furkanpasaoglu@users.noreply.github.com>
2021-02-13 22:47:45 +0300

    13 Şubat 1.commit

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //Attribute 
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //Naming convention
+        //IoC Container -- Inversion of control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency chain --
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
